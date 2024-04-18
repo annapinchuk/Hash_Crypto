370d659ffadd4879130ee6c79d78470cf0ae9704
annapinchuk <76442355+annapinchuk@users.noreply.github.com>
2024-04-18 18:28:30 +0300

    Modified the flexibility of h & p
    Modified the flexibility of h & p

--- hash.py
+++ hash.py
@@ -1,13 +1,32 @@
 import random
 
-# get random INTEGER from the Cyclic group and check if it is a generator of the group, if not get another random integer
-def random_generator(p):
+def is_prime(n):
+    """ Check if a number is a prime number """
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def is_generator(g, p):
+    """ Check if g is a generator of the cyclic group Z_p* """
+    return set(pow(g, i, p) for i in range(1, p)) == set(range(1, p))
+
+def random_generator(p, exclude_h=None):
     g = random.randint(2, p-1)
-    print("Random g:", g)
-    # while g is not a generator of p or  = 31, get another random integer
-    while not is_generator(g, p) or g == 31:
+    print("Initial random g:", g)
+    while not is_generator(g, p) or g == exclude_h:
         g = random.randint(2, p-1)
-    # print all the pows of g to check if it is a generator of p
+        print("New random g:", g)
+    
+    # Print all powers of g
     g_powers = {}
     for i in range(1, p):
         pow_g_i = pow(g, i, p)
@@ -15,26 +34,30 @@ def random_generator(p):
     print("Powers of g for each member:")
     for member in sorted(g_powers):
         print(f"{member} = {g} ^ {g_powers[member]} (mod {p})")
-    return g
- 
-# check if g is a generator of p
-def is_generator(g, p):
-    return set(pow(g, i, p) for i in range(1, p)) == set(range(1, p))
 
+    return g
 
 def hash_function(a, b, g, p, h):
     return (pow(g, a, p) * pow(h, b, p)) % p
 
 def main():
-    p = 101
+    while True:
+        p = int(input("Enter a prime number p: "))
+        if is_prime(p):
+            break
+        print("The number entered is not a prime. Please enter a prime number.")
+    
     g = random_generator(p)
-    #check if g is a generator
     if not is_generator(g, p):
         print("Error: g is not a generator")
         return
-    h = 31
 
-    # ask for a and b
+    while True:
+        h = int(input("Enter a value for h that is not a generator of Z_p*: "))
+        if h >= 2 and h < p and not is_generator(h, p):
+            break
+        print("The value for h is either not within range, is a generator, or is invalid. Please enter another value for h.")
+
     a = int(input("Enter a: "))
     b = int(input("Enter b: "))
 
